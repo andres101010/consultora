b24fe97732ca68a873ae93ce2fcdbc6cd6a44d0b
andres101010 <andreschapu5295@gmail.com>
2023-02-08 00:11:19 -0400

    fase final

--- src/pages/constactos/Contactos.jsx
+++ src/pages/constactos/Contactos.jsx
@@ -1,5 +1,64 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
+import { BsFillGeoAltFill } from "react-icons/bs";
+import{ BsWhatsapp } from "react-icons/bs"
 export const Contantos = ()=>{
     return(
-        <div>Contanto</div>
+        <div>
+            <h1 className="text-center text-white m-5">Contactenos</h1>
+            <Container>
+            <Row>
+        <Col md={6}>
+        <Card className='d-block w-100'>
+      <Card.Img variant="top" src="src\assets\img\location-162102_1280-550x500.png" className='img-location' />
+      <Card.Body>
+        <Card.Title>Localizacion</Card.Title>
+        <Card.Text>
+            <p>Nos puede localizar en la siguiente direccion:</p>
+         <BsFillGeoAltFill />calle Santa Fe 502 #- San Miguel De Tucumán
+        </Card.Text>
+      </Card.Body>
+    </Card>
+        </Col>
+       
+        <Col  md={6}>
+        <Card className='d-block w-100'>
+      <Card.Img  variant="top" src="src\assets\img\whatsapp_web_descargar_varios_archivos_al_mismo_tiempo.webp" />
+      <Card.Body>
+        <Card.Title>Whatsapp</Card.Title>
+        <Card.Text>
+         <p>Puedes comunicarte con nosotros via whatsapp.</p>
+           <BsWhatsapp /> <a href="+54 9 381-5459826">+54 9 381-5459826</a> <br />
+           <BsWhatsapp /> <a href="+54 9 381-6071866">+54 9 381-6071866</a>
+         
+        </Card.Text>
+      </Card.Body>
+    </Card>
+        </Col>
+      </Row>
+      </Container>
+      <footer className='footer-contacto'>
+      <h4 className='text-white m-5 '>Seguinos</h4>
+      <ul className='text-white m-3'>
+        <li> <Link to={'https://www.facebook.com/profile.php?id=100054477969189'} target='_blank'><img src="src\assets\img\facebook.jpg" alt="Facebook" style={{width:'50px'}}/></Link>
+        </li>
+        <li>
+            <Link to={'https://www.linkedin.com/company/abzu-mining-exploration/about/'} target='_blank'><img src="src\assets\img\LinkedIn_logo_initials.png" alt="Linkedin" style={{width:'50px'}}/></Link>
+        </li>
+      </ul>
+        <h4 className='text-white m-5'>Servicios</h4>
+        <ul className='text-white'>
+            <li>Estrategia y gestión de proyectos</li>
+            <li>Exploración mineral</li>
+            <li>Control y Garantia de calidad de los datos</li>
+
+        </ul>
+        <h4 className='text-white m-5'>Nosotros</h4>
+        <p className='text-white m-4'>ABZU nace con el objetivo de ofrecer asesoramiento y orientación en operaciones de inversión y factibilidad de proyectos, análisis de riesgos, evaluación y modelización de recursos, planificación y decisiones estratégicas.</p>
+      </footer>
+        </div>
     )
 }
--- src/pages/proyectos/Proyectos.jsx
+++ src/pages/proyectos/Proyectos.jsx
@@ -1,8 +1,32 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 export const Proyectos = ()=>{
     return(
         <div>
             <h1 className="text-center text-white m-3">Proyectos</h1>
-            
+    <Container>
+      <Row>
+        <Col md={12} style={{border:'double', borderColor:'darkgray', borderRadius:'5px',margin:'5px'}}>
+            <img src="src\assets\img\imagen Carla I. 2bis.jpg" alt="carla1.bis" className='d-block w-100 mb-3'/>
+            <p className='text-canter text-white'>La propiedad Carla I se encuentra en el extremo sur de un prominente corredor estructural NE-SW Zona 
+               de transferencia de Tucum&aacute;n, que ha sido identificada como importante en la historia del emplazamiento
+               de los centros de p&oacute;rfido del Mioceno superior Cu-An (Mo) (por ejemplo, Bajo de la Alumbrera y Agua Rica),
+               en esta parte de Argentina. Hasta la fecha, no se han realizado citas por edades en la propiedad de Carla I y no se
+               han publicado fechas de edad.
+            </p>
+            <span className='text-center text-success'>"Carla I"</span>
+        </Col>
+        <Col md={12} style={{border:'double', borderColor:'darkgray', borderRadius:'5px',margin:'5px'}}>
+        <img src="src\assets\img\imagen Carla I.bis.jpg" alt="carla1" className='d-block w-100 mb-3'/>
+        <p className='text-canter text-white'>The “Carla I” property claim includes 2,000 has and is located in the Belen department of the Catamarca Province of NW Argentina. 
+        Access to the general area is good via the paved highway (Ruta 40) that connects the city of Catamarca, from the south, with the small town of Londres, 
+        located Approximately 7.5 km to the east of the property</p>
+        <span className='text-center text-success'>"Carla I"</span>
+        </Col>
+        
+      </Row>
+    </Container>
         </div>
     )
 }
